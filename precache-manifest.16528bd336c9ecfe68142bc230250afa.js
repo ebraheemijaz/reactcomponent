97440cafe2b92c9cdc2347cb33011738a6d4e9e0
Ebraheem <ebraheemijaz913@gmail.com>
2020-07-01 20:44:30 +0500

    Updates

---
+++ precache-manifest.16528bd336c9ecfe68142bc230250afa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b60b1e2241f754f8308bd5fa3344d07f",
+    "url": "/reactcomponent/index.html"
+  },
+  {
+    "revision": "93a736e7cbf30880c707",
+    "url": "/reactcomponent/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "c4a5b99566f54208bfa5",
+    "url": "/reactcomponent/static/js/2.e343d3d6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/reactcomponent/static/js/2.e343d3d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93a736e7cbf30880c707",
+    "url": "/reactcomponent/static/js/main.769dc040.chunk.js"
+  },
+  {
+    "revision": "16c538b5b9417f70a1d1",
+    "url": "/reactcomponent/static/js/runtime-main.3f268ea5.js"
+  }
+]);
